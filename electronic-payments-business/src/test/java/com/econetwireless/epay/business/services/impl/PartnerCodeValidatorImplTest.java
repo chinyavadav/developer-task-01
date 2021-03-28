829059832a5b726d8ac3e6de2d9aa6a3ea552837
chinyavadav <chinyavadav@gmail.com>
2021-03-28 18:44:08 +0200

    PartnerCodeValidatorImplTest

--- electronic-payments-business/src/test/java/com/econetwireless/epay/business/services/impl/PartnerCodeValidatorImplTest.java
+++ electronic-payments-business/src/test/java/com/econetwireless/epay/business/services/impl/PartnerCodeValidatorImplTest.java
@@ -1,16 +1,44 @@
 package com.econetwireless.epay.business.services.impl;
 
+import com.econetwireless.epay.dao.requestpartner.api.RequestPartnerDao;
+import com.econetwireless.epay.dao.subscriberrequest.api.SubscriberRequestDao;
+import com.econetwireless.epay.domain.RequestPartner;
+import com.econetwireless.epay.domain.SubscriberRequest;
+import com.econetwireless.utils.enums.ResponseCode;
+import com.econetwireless.utils.execeptions.EpayException;
 import junit.framework.TestCase;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
-public class PartnerCodeValidatorImplTest extends TestCase {
+import java.util.Collections;
 
-    public void setUp() throws Exception {
-        super.setUp();
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class PartnerCodeValidatorImplTest {
+
+    private String partnerCode;
+
+    @Mock
+    private RequestPartnerDao requestPartnerDao;
+
+    @Before
+    public void init() {
+        partnerCode = "hot-recharge";
+        RequestPartner requestPartner = new RequestPartner();
+        requestPartner.setId(123l);
+        requestPartner.setCode(partnerCode);
+        requestPartner.setName("Hot Recharge");
+        requestPartner.setDescription("HOT RECHARGE PLATFORM");
+        when(requestPartnerDao.findByCode(partnerCode)).thenReturn(requestPartner);
     }
 
-    public void tearDown() throws Exception {
-    }
-
+    @Test
     public void testValidatePartnerCode() {
+        assertTrue(requestPartnerDao.findByCode(partnerCode) != null);
     }
 }
