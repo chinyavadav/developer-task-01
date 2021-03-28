e4a9391152f2d8dd3a7ede4d2366634762f3ba81
chinyavadav <chinyavadav@gmail.com>
2021-03-28 19:40:16 +0200

    EnquiriesServiceImplTest

--- electronic-payments-business/src/test/java/com/econetwireless/epay/business/services/impl/CreditsServiceImplTest.java
+++ electronic-payments-business/src/test/java/com/econetwireless/epay/business/services/impl/CreditsServiceImplTest.java
@@ -2,18 +2,7 @@ package com.econetwireless.epay.business.services.impl;
 
 import junit.framework.TestCase;
 
-/**
- * Created by chinyavadav on 28/3/2021.
- */
-//@RunWith(SpringJUnit4ClassRunner.class)
-//@WebAppConfiguration
-//@ContextHierarchy({
-//        @ContextConfiguration(classes = {RootConfig.class}),
-//})
 public class CreditsServiceImplTest extends TestCase {
-
-    public void testCredit() {
-    }
 
     public void setUp() throws Exception {
         super.setUp();
@@ -22,6 +11,6 @@ public class CreditsServiceImplTest extends TestCase {
     public void tearDown() throws Exception {
     }
 
-    public void testTestCredit() {
+    public void testCredit() {
     }
 }
--- electronic-payments-business/src/test/java/com/econetwireless/epay/business/services/impl/EnquiriesServiceImplTest.java
+++ electronic-payments-business/src/test/java/com/econetwireless/epay/business/services/impl/EnquiriesServiceImplTest.java
@@ -1,39 +1,56 @@
 package com.econetwireless.epay.business.services.impl;
 
+import com.econetwireless.epay.dao.subscriberrequest.api.SubscriberRequestDao;
 import com.econetwireless.epay.domain.SubscriberRequest;
+import com.econetwireless.utils.constants.SystemConstants;
 import com.econetwireless.utils.enums.ResponseCode;
 import com.econetwireless.utils.messages.AirtimeBalanceResponse;
 import com.econetwireless.utils.pojo.INBalanceResponse;
 import junit.framework.TestCase;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
+
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
 public class EnquiriesServiceImplTest extends TestCase {
 
-    private static final String partnerCode = "hot-recharge";
-    private static final String msisdn = "263782135087";
-    SubscriberRequest subscriberRequest;
+    private String partnerCode, msisdn;
+    private SubscriberRequest initialSubscriberRequest;
+
+    @Mock
+    private SubscriberRequestDao subscriberRequestDao;
 
     @Before
     public void init() {
-        subscriberRequest = new SubscriberRequest();
+        partnerCode = "hot-recharge";
+        msisdn = "263782135087";
+        initialSubscriberRequest = populate(partnerCode, msisdn);
+
+        SubscriberRequest subscriberRequest = new SubscriberRequest();
         subscriberRequest.setId(123l);
         subscriberRequest.setRequestType("Airtime Balance Enquiry");
         subscriberRequest.setPartnerCode(partnerCode);
         subscriberRequest.setMsisdn("263782135087");
         subscriberRequest.setAmount(100);
+        when(subscriberRequestDao.save(initialSubscriberRequest)).thenReturn(subscriberRequest);
+
     }
 
     @Test
     public void testEnquire() {
         final AirtimeBalanceResponse airtimeBalanceResponse = new AirtimeBalanceResponse();
-
+        final SubscriberRequest createdSubscriberRequest = subscriberRequestDao.save(initialSubscriberRequest);
         final INBalanceResponse inBalanceResponse = new INBalanceResponse();
         inBalanceResponse.setResponseCode(ResponseCode.SUCCESS.getCode());
         inBalanceResponse.setMsisdn(msisdn);
         inBalanceResponse.setAmount(100);
         inBalanceResponse.setNarrative("Airtime Purchase");
-
+        changeSubscriberStateOnBalanceEnquiry(createdSubscriberRequest, inBalanceResponse);
         airtimeBalanceResponse.setResponseCode(inBalanceResponse.getResponseCode());
         airtimeBalanceResponse.setNarrative(inBalanceResponse.getNarrative());
         airtimeBalanceResponse.setMsisdn(msisdn);
@@ -42,5 +59,24 @@ public class EnquiriesServiceImplTest extends TestCase {
         assertEquals(airtimeBalanceResponse.getResponseCode(), "200");
         assertEquals(airtimeBalanceResponse.getAmount(), 100d);
         assertEquals(airtimeBalanceResponse.getMsisdn(), "263782135087");
+    }
+
+    private static void changeSubscriberStateOnBalanceEnquiry(final SubscriberRequest subscriberRequest, final INBalanceResponse inBalanceResponse) {
+        final boolean isSuccessfulResponse = ResponseCode.SUCCESS.getCode().equalsIgnoreCase(inBalanceResponse.getResponseCode());
+        if (!isSuccessfulResponse) {
+            subscriberRequest.setStatus(SystemConstants.STATUS_FAILED);
+        } else {
+            subscriberRequest.setStatus(SystemConstants.STATUS_SUCCESSFUL);
+            subscriberRequest.setBalanceAfter(inBalanceResponse.getAmount());
+            subscriberRequest.setBalanceBefore(inBalanceResponse.getAmount());
+        }
+    }
+
+    private static SubscriberRequest populate(final String partnerCode, final String msisdn) {
+        final SubscriberRequest subscriberRequest = new SubscriberRequest();
+        subscriberRequest.setRequestType(SystemConstants.REQUEST_TYPE_AIRTIME_BALANCE_ENQUIRY);
+        subscriberRequest.setPartnerCode(partnerCode);
+        subscriberRequest.setMsisdn(msisdn);
+        return subscriberRequest;
     }
 }
